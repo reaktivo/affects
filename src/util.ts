0c4e08ed71e848149878a51a1bbd593daf5637b8
Marcel Miranda <m@reaktivo.com>
2021-03-28 23:08:56 +0200

    Fix lint

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
 export function mergeMaps(...maps: Array<Map<unknown, unknown> | unknown>) {
   const mergedMap = new Map();
-  for(const map of maps) {
+  for (const map of maps) {
     if (map instanceof Map) {
       map.forEach((value, key) => {
         mergedMap.set(key, value);
